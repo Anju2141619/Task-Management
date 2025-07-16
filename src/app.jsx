2e84cb1ed152df29759e415951b4fb88795a78b2
Anju2141619 <panju9363@gmail.com>
2025-07-16 09:40:34 +0545

    Debug

--- src/app.jsx
+++ src/app.jsx
@@ -4,44 +4,18 @@ import Filter from "./components/filter";
 import TaskList from "./components/tasklist";
 import TaskForm from "./components/taskform";
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState("All");
+const addTask = (task) => {
+  setTasks([...tasks, task]);
+};
 
-  const addTask = (task) => {
-    setTasks([...tasks, task]);
-  };
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id));
+};
 
-  const toggleTask = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const filteredTasks = tasks.filter((task) => {
-    if (filter === "All") return true;
-    if (filter === "Active") return !task.completed;
-    if (filter === "Completed") return task.completed;
-  });
-
-  return (
-    <div style={{ maxWidth: "600px", margin: "auto", padding: "20px" }}>
-      <Header />
-      <TaskForm onAdd={addTask} />
-      <Filter current={filter} onChange={setFilter} />
-      <TaskList
-        tasks={filteredTasks}
-        onToggle={toggleTask}
-        onDelete={deleteTask}
-      />
-    </div>
+const toggleTask = (id) => {
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    )
   );
-}
-
-export default App;
+};
